b2530fcfc855900976b89baef28ca83b573872e9
Krzysztof Kata <254776@edu.p.lodz.pl>
2026-03-16 23:15:29 +0100

    Metoda do permutacji w algorytmie DES

--- zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
+++ zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
@@ -1,4 +1,15 @@
 package pl.desx.cryptography;
 
 public class DesAlgorithm {
+
+    public long permute(long input_plain_block, byte[] table_bytes, int bytes_to_input){
+        long output_plain_block = 0;
+        for (int i=0; i<table_bytes.length; i++){
+            output_plain_block <<= 1;
+            int bit_position = table_bytes[i];
+            long extracted_bit = (bytes_to_input - bit_position) & 1L;
+            output_plain_block |= extracted_bit;
+        }
+        return output_plain_block;
+    }
 }
