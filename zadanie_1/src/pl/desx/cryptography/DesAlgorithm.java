6b115dce886b93a261e80f6cb347a1833f9507de
Krzysztof Kata <254776@edu.p.lodz.pl>
2026-03-17 20:22:25 +0100

    Zmiana nazewnictwa

--- zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
+++ zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
@@ -2,12 +2,12 @@ package pl.desx.cryptography;
 
 public class DesAlgorithm {
 
-    public long permute(long input_plain_block, byte[] table_bytes, int bytes_to_input){
+    public long permute(long input_plain_block, byte[] table_bytes, int important_bytes){
         long output_plain_block = 0;
         for (int i=0; i<table_bytes.length; i++){
             output_plain_block <<= 1;
             int bit_position = table_bytes[i];
-            long extracted_bit = (input_plain_block >>> (bytes_to_input - bit_position)) & 1L;
+            long extracted_bit = (input_plain_block >>> (important_bytes - bit_position)) & 1L;
             output_plain_block |= extracted_bit;
         }
         return output_plain_block;
