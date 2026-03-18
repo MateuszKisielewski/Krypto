8e1e513342e367e3eac4b1dc1f426a5c80b75cdf
Krzysztof Kata <254776@edu.p.lodz.pl>
2026-03-18 10:33:14 +0100

    Gotowe generowanie podkluczy

--- zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
+++ zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
@@ -17,10 +17,50 @@ public class DesAlgorithm {
         long[] LPT_RPT = new long[2];
         LPT_RPT[0] = output_IP >>> 32; //Wywala te 32 bity z prawej
         LPT_RPT[1] = (output_IP << 32) >>> 32; //Wywala te 32 bity z lewej i przesuwa spowrotem na prawo, lewa strona to same zera
-        return LPT_RPT; //pomyśleć jak zwrócić bo java niby nie obsługuje dwóch returnow
+        return LPT_RPT; //zwracamy tablicę w której mamy lewą i prawą część osobno
     }
 
     public long XOR(long LPT, long fourSteppedRPT){
         return LPT ^ fourSteppedRPT; //po prostu xor z LPT i z tego RPT po tych 4 krokach
     }
+    public long[] generate_sub_keys(long key_64) {
+        long[] sub_keys = new long[16];
+
+        long key_56 = permute(key_64, DesConstants.permuted_choice_1, 64);
+
+        long left_key_28 = key_56 >>> 28;
+        long right_key_28 = (key_56 << 36) >>> 36;
+        int shift = 0;
+
+        for (int i = 1; i<=16; i++){
+            if (i == 1 || i == 2 || i == 9 || i == 16){
+                shift = 1;
+            }
+            else {
+                shift = 2;
+            }
+
+            long temp_bit = left_key_28 >>> (28 - shift);
+            left_key_28 <<= shift;
+            left_key_28 &= 0xFFFFFFFL;
+            left_key_28 |= temp_bit;
+
+            temp_bit = right_key_28 >>> (28 - shift);
+            right_key_28 <<= shift;
+            right_key_28 &= 0xFFFFFFFL;
+            right_key_28 |= temp_bit;
+
+            long shifted_key_56 = left_key_28;
+            shifted_key_56 <<= 28;
+            shifted_key_56 |= right_key_28;
+
+            long key_48 = permute(shifted_key_56, DesConstants.permuted_choice_2, 56);
+
+            sub_keys[i-1] = key_48;
+        }
+
+        return sub_keys;
+    }
+
+
 }
