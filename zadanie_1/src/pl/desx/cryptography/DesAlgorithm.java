1229977cf47b17af9ba5d4fd29760d45b9a01a82
Mateusz Kisielewski <254779@edu.p.lodz.pl>
2026-03-18 09:40:12 +0100

    some methods added

--- zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
+++ zadanie_1/src/pl/desx/cryptography/DesAlgorithm.java
@@ -2,14 +2,25 @@ package pl.desx.cryptography;
 
 public class DesAlgorithm {
 
-    public long permute(long input_plain_block, byte[] table_bytes, int important_bytes){
-        long output_plain_block = 0;
+    public long permute(long input_plain_text, byte[] table_bytes, int important_bytes){
+        long output_plain_text = 0;
         for (int i=0; i<table_bytes.length; i++){
-            output_plain_block <<= 1;
+            output_plain_text <<= 1;
             int bit_position = table_bytes[i];
-            long extracted_bit = (input_plain_block >>> (important_bytes - bit_position)) & 1L;
-            output_plain_block |= extracted_bit;
+            long extracted_bit = (input_plain_text >>> (important_bytes - bit_position)) & 1L;
+            output_plain_text |= extracted_bit;
         }
-        return output_plain_block;
+        return output_plain_text;
+    }
+
+    public long[] split_LPT_RPT(long output_IP) {
+        long[] LPT_RPT = new long[2];
+        LPT_RPT[0] = output_IP >>> 32; //Wywala te 32 bity z prawej
+        LPT_RPT[1] = (output_IP << 32) >>> 32; //Wywala te 32 bity z lewej i przesuwa spowrotem na prawo, lewa strona to same zera
+        return LPT_RPT; //pomyśleć jak zwrócić bo java niby nie obsługuje dwóch returnow
+    }
+
+    public long XOR(long LPT, long fourSteppedRPT){
+        return LPT ^ fourSteppedRPT; //po prostu xor z LPT i z tego RPT po tych 4 krokach
     }
 }
