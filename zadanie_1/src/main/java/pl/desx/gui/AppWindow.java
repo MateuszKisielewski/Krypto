ef9758ed77b5898c065de993324aa34cf3ac242a
Mateusz Kisielewski <254779@edu.p.lodz.pl>
2026-03-19 17:44:01 +0100

    poczatekMenu

--- zadanie_1/src/main/java/pl/desx/gui/AppWindow.java
+++ zadanie_1/src/main/java/pl/desx/gui/AppWindow.java
@@ -1,11 +1,88 @@
 package pl.desx.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import pl.desx.cryptography.DesAlgorithm;
+import pl.desx.cryptography.DesxAlgorithm;
 
 public class AppWindow {
 
     @FXML
-    public void onButtonClicked() {
-        System.out.println("Działa! Zaczynamy algorytm DESX!");
+    private ToggleGroup choice;
+
+    @FXML
+    private RadioButton deszyfrujButton;
+
+    @FXML
+    private Button generujKlucze;
+
+    @FXML
+    private TextField kluczDrugi;
+
+    @FXML
+    private TextField kluczPierwszy;
+
+    @FXML
+    private TextField kluczTrzeci;
+
+    @FXML
+    private Button start;
+
+    @FXML
+    private RadioButton szyfrujButton;
+
+    @FXML
+    private TextArea tekstJawny;
+
+    @FXML
+    private TextArea tekstZaszyfrowany;
+
+    @FXML
+    private Button wczytajKlucze;
+
+    @FXML
+    private Button wczytajPlik;
+
+    @FXML
+    private Button zapiszKlucze;
+
+    @FXML
+    private Button zapiszPlik;
+
+    @FXML
+    void onGenerujKlucze(ActionEvent event) {
+        DesxAlgorithm des = new DesxAlgorithm();
+        des.generate_keys();
     }
+
+    @FXML
+    void onStart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onWczytajKlucze(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onWczytajPlik(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onZapiszKlucze(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onZapiszPlik(ActionEvent event) {
+
+    }
+
 }
