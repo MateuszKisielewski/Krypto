8c2b3d59e88fd8d57cfcaf5982fbb7c3aa0b986d
Krzysztof Kata <254776@edu.p.lodz.pl>
2026-03-19 10:21:03 +0100

    DESX done

--- zadanie_1/src/main/java/pl/desx/cryptography/DesxAlgorithm.java
+++ zadanie_1/src/main/java/pl/desx/cryptography/DesxAlgorithm.java
@@ -3,5 +3,35 @@ package pl.desx.cryptography;
 import java.security.SecureRandom;
 
 public class DesxAlgorithm {
+   private final SecureRandom sr = new SecureRandom();
+
+    private long key_1;
+    private long key_2;
+    private long key_3;
+
+    public void generate_keys() {
+        this.key_1 = sr.nextLong();
+        this.key_2 = sr.nextLong();
+        this.key_3 = sr.nextLong();
+    }
+
+    public long main_desx_encrypt(long plain_text) {
+        DesAlgorithm des = new DesAlgorithm();
+        long first_xor = key_1 ^ plain_text;
+        long des_result = des.main_encrypt_algorithm(first_xor, key_2);
+        long second_xor = key_3 ^ des_result;
+
+        return second_xor;
+    }
+
+    public long main_desx_decrypt(long cipher_text) {
+        DesAlgorithm des = new DesAlgorithm();
+        long first_decrypt = key_3 ^ cipher_text;
+        long second_decrypt = des.main_decrypt_algorithm(first_decrypt, key_2);
+        long third_decrypt = key_1 ^ second_decrypt;
+
+        return third_decrypt;
+    }
+
 
 }
