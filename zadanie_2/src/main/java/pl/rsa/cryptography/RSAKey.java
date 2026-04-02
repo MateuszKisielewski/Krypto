4b45e4ed2d6d2896d818de45df69b68af9ab6e43
KrzychuToJaa <krzysztofkata0@gmail.com>
2026-04-02 14:36:03 +0200

    Generowanie klczu

--- zadanie_2/src/main/java/pl/rsa/cryptography/RSAKey.java
+++ zadanie_2/src/main/java/pl/rsa/cryptography/RSAKey.java
@@ -1,4 +1,27 @@
 package pl.rsa.cryptography;
 
+import java.math.BigInteger;
+
 public class RSAKey {
+    public BigInteger e;
+    public BigInteger d;
+    public BigInteger n;
+
+    public RSAKey(BigInteger e, BigInteger d, BigInteger n) {
+        this.e = e;
+        this.d = d;
+        this.n = n;
+    }
+
+    public BigInteger getE() {
+        return e;
+    }
+
+    public BigInteger getD() {
+        return d;
+    }
+
+    public BigInteger getN() {
+        return n;
+    }
 }
--- zadanie_2/src/main/java/pl/rsa/cryptography/RSAKeyGenerator.java
+++ zadanie_2/src/main/java/pl/rsa/cryptography/RSAKeyGenerator.java
@@ -1,4 +1,26 @@
 package pl.rsa.cryptography;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class RSAKeyGenerator {
+    SecureRandom rnd = new SecureRandom();
+    
+    RSAKey generateRSAKey(int bitLenght) {
+        BigInteger p = BigInteger.probablePrime(bitLenght / 2, rnd);
+        BigInteger q = BigInteger.probablePrime(bitLenght / 2, rnd);
+
+        BigInteger n = p.multiply(q);
+        BigInteger e = BigInteger.valueOf(65537);
+
+        BigInteger eulerFunction = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
+        while (!e.gcd(eulerFunction).equals(BigInteger.ONE)) {
+            e = e.add(BigInteger.TWO);
+        }
+
+        BigInteger d = e.modInverse(eulerFunction);
+        
+        return new RSAKey(e,d,n);
+    }
 }
