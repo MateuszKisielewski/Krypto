90cc2b155114f9feb5a223a23effcf57974fc7d7
KrzychuToJaa <krzysztofkata0@gmail.com>
2026-04-04 21:25:05 +0200

    Funkcja do zaślepiania klucza

--- zadanie_2/src/main/java/pl/rsa/cryptography/BlindSignature.java
+++ zadanie_2/src/main/java/pl/rsa/cryptography/BlindSignature.java
@@ -1,4 +1,23 @@
 package pl.rsa.cryptography;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class BlindSignature {
+    private final SecureRandom sr = new SecureRandom();
+
+    BigInteger[] blindingText(BigInteger m, BigInteger e, BigInteger n){
+        BigInteger r;
+
+        do {
+            r = new BigInteger(n.bitLength(), sr);
+        }
+        while (r.compareTo(BigInteger.ONE) <= 0 || r.compareTo(n) >= 0 || !r.gcd(n).equals(BigInteger.ONE));
+
+        BigInteger rToPowerOFe = r.modPow(e, n);
+
+        BigInteger blindText = m.multiply(rToPowerOFe).mod(n);
+
+        return new BigInteger[]{blindText, r};
+    }
 }
