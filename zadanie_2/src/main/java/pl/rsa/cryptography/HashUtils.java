e01bdf223f26c2072ba1184c829a576daa28f4ec
KrzychuToJaa <krzysztofkata0@gmail.com>
2026-04-04 22:05:01 +0200

    hasowanie dostępnej wiadomości

--- zadanie_2/src/main/java/pl/rsa/cryptography/HashUtils.java
+++ zadanie_2/src/main/java/pl/rsa/cryptography/HashUtils.java
@@ -1,4 +1,14 @@
 package pl.rsa.cryptography;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashUtils {
+    public BigInteger dataToHash (byte[] data) throws NoSuchAlgorithmException {
+        MessageDigest hashData;
+        hashData = MessageDigest.getInstance("SHA-256");
+        byte[] hashedData = hashData.digest(data);
+        return new BigInteger(1, hashedData);
+    }
 }
